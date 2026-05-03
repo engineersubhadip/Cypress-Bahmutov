a1b416a2c45261fa50dfa2efb02b930db43322d1
Subhadip Das <subhadip.das02@ad.infosys.com>
2026-05-03 14:10:56 +0530

    Introduction to cypress-map plugin

--- cypress/e2e/prices.cy.js
+++ cypress/e2e/prices.cy.js
@@ -1,4 +1,6 @@
-it("confirms the item with the lowest price", () => {
+import "cypress-map";
+
+it.skip("confirms the item with the lowest price : Non-Optimized", () => {
   cy.visit("/");
   // Tip: grab the username and the password from the login page
   // It is ok for now to hardcode it in the spec source here
@@ -29,5 +31,24 @@ it("confirms the item with the lowest price", () => {
       return Cypress._.min(priceArr);
     })
     .then(console.log)
+    .should("equal", 7.99);
+});
+
+it("Confirms the items with the lowest price : Optimized", () => {
+  cy.visit("/");
+  cy.get('[data-test="username"]').type("standard_user");
+  // get the password field and type the password
+  cy.get('[data-test="password"]').type("secret_sauce");
+  // get the login button and click on it
+  // https://on.cypress.io/click
+  cy.get('[data-test="login-button"]').click();
+  // * Verify we have landed on the new url
+  cy.location("pathname").should("equal", "/inventory.html");
+  cy.get(".pricebar  > .inventory_item_price")
+    .should("have.length", 6)
+    .map("innerText")
+    .mapInvoke("slice", 1)
+    .map(Number)
+    .apply(Cypress._.min)
     .should("equal", 7.99);
 });
