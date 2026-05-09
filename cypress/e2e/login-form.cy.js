22a9752f53cef2266b5861c05b4ac1f3a5d44eff
Subhadip Das <subhadip.das02@ad.infosys.com>
2026-05-09 11:46:02 +0530

    Validated the scenario where the user validates the error message, after entering half details and trying to login

--- cypress/e2e/login-form.cy.js
+++ cypress/e2e/login-form.cy.js
@@ -1,4 +1,24 @@
+import Login from "./pageObjects/login";
+
+const login = new Login();
+
 describe("Login form", () => {
-  it("shows an error message for empty username", () => {});
-  it("shows an error message for empty password", () => {});
+  beforeEach(() => {
+    cy.visit("/");
+  });
+  it("shows an error message for empty username", () => {
+    login.clickOnLoginButton();
+    login
+      .getErrorMessage()
+      .should("be.visible")
+      .and("include.text", "Epic sadface: Username is required");
+  });
+  it.only("shows an error message for empty password", () => {
+    login.enterUserName("name");
+    login.clickOnLoginButton();
+    login
+      .getErrorMessage()
+      .should("be.visible")
+      .and("include.text", "Epic sadface: Password is required");
+  });
 });
